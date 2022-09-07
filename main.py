a1674acfbcceb2558fd37df8b8b4cab2368a9eab
Neb <merlinx50@gmail.com>
2022-09-07 16:36:19 -0400

    adjusted variance

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def calc_sales(g1, g2, g3, game_age):
     score_avg = np.average(score_results)
     score_var = np.max(score_results) - np.min(score_results)
     sales_avg = np.average(sale_results)
-    sales_var = np.max(sale_results) - np.min(sale_results)
+    sales_var = abs(np.max(sale_results) - np.min(sale_results) - sales_avg)
     pred_results = {'AvgAcc': score_avg, 'AvgSales': sales_avg,
     'SalesVar': sales_var, 'AccVar': score_var}
     display_results(pred_results)
